7e4265c2e1cad5fac7983f3baa563e8ee5c97613
arashmad <arashmad@gfz-potsdam.de>
2022-01-18 17:00:03 +0100

    Usless links were removed.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/config.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/config.py
@@ -13,9 +13,6 @@ DB_CONNECTION_URL = connection_url
 class Settings(BaseSettings):
     API_V1_STR: str = ""
 
-    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
-    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
-    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)

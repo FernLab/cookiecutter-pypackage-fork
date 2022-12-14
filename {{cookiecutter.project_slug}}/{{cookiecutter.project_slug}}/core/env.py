9acc6ef72b13fde4d4a043ae3b5afb02596922f7
arashmad <arashmad@gfz-potsdam.de>
2022-12-14 13:46:36 +0100

    cleaned. create data_dirwhen app starts

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/env.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/env.py
@@ -6,17 +6,9 @@
 
 import os
 
-from {{ cookiecutter.project_slug }}.core.config_parser import config
-
-APP_DOMAIN = config['app']['app_address']
-
-LEVEL_OF_DEVELOPMENT = ''
-if 'level_of_development' in os.environ:  # pragma: no cover
-    LEVEL_OF_DEVELOPMENT = os.environ['level_of_development']
-    APP_DOMAIN = 'localhost'
-
-if 'server_name' in os.environ:  # pragma: no cover
-    APP_DOMAIN = os.environ['server_name']
+if 'DATA_DIRECTORY' in os.environ:
+    if not os.path.isdir(os.environ['DATA_DIRECTORY']):
+        os.mkdir(os.environ['DATA_DIRECTORY'])
 
 SERVICE_NAMESPACE = ''
 if 'service_namespace' in os.environ:  # pragma: no cover

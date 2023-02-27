73db3e78998e7cf38f378efdc2af04dd65605c94
arashmad <arashmad@gfz-potsdam.de>
2023-02-27 15:10:12 +0100

    removed constant names and used slug name

--- {{cookiecutter.project_slug}}/run.py
+++ {{cookiecutter.project_slug}}/run.py
@@ -12,61 +12,74 @@ if __name__ == "__main__":
     data_dir_in = None
     try:
         application_scope = os.environ['API_SERVER_SCOPE']
-        if application_scope == 'DEPLOYMENT':
+    except:
+        # Here is for
+        # python run.py
+        load_dotenv()
+        try:
+             application_scope = os.getenv('API_SERVER_SCOPE')
+             
+        except:
+            raise IOError(f'Undefined SCOPE for the service. Environment <API_SERVER_SCOPE> should be set.')
+
+    if application_scope not in ['DEPLOYMENT', 'DEVELOPMENT']:
+        raise ValueError(f'Unsupported "{application_scope}" for the <API_SERVER_SCOPE> environment.')
+    
+    # Here is for
+    # docker-compose up
+    if application_scope == 'DEPLOYMENT':
+        try:
+            PORT = int(os.environ['PORT'])
+            if not PORT:
+                raise EnvironmentError('<PORT> environment needs to be set.')
+        except:
+            raise EnvironmentError('Check .env file to see if you set <DATA_DIR> environment.')
+        try:
+            data_dir = os.environ['DATA_DIR']
+            if not data_dir:
+                raise EnvironmentError('<DATA_DIR> environment needs to be set.')
+        except:
+            raise EnvironmentError('Check .env file to see if you set <DATA_DIR> environment.')
+
+    # Here is for
+    # IDE debugger
+    elif application_scope == 'DEVELOPMENT':
             try:
-                PORT = int(os.environ['PORT'])
+                PORT = int(os.getenv('PORT'))
                 if not PORT:
                     raise EnvironmentError('<PORT> environment needs to be set.')
             except:
                 raise EnvironmentError('Check .env file to see if you set <DATA_DIR> environment.')
-            try:
-                data_dir = os.environ['DATA_DIR']
-                if not data_dir:
-                    raise EnvironmentError('<DATA_DIR> environment needs to be set.')
-            except:
-                raise EnvironmentError('Check .env file to see if you set <DATA_DIR> environment.')
-
-        # In debug mode 
-        elif application_scope == 'DEVELOPMENT':
-                try:
-                    PORT = int(os.getenv('PORT'))
-                    if not PORT:
-                        raise EnvironmentError('<PORT> environment needs to be set.')
-                except:
-                    raise EnvironmentError('Check .env file to see if you set <DATA_DIR> environment.')
-                data_dir = os.path.join(os.getcwd(), 'data_dir')
-        else:
-           raise ValueError(f'Unsupported value for the <API_SERVER_SCOPE> environment => {application_scope}')
+            data_dir = os.path.join(os.getcwd(), 'data_dir')
         
-    except:
-        # In case of using python run.py
-        try:
-            load_dotenv()
-            application_scope = os.getenv('API_SERVER_SCOPE')
+    # except:
+    #     # In case of using python run.py
+    #     try:
+    #         load_dotenv()
+    #         application_scope = os.getenv('API_SERVER_SCOPE')
 
-            if application_scope == 'DEVELOPMENT':
-                try:
-                    PORT = int(os.getenv('PORT'))
-                    if not PORT:
-                        raise EnvironmentError('<PORT> environment needs to be set.')
-                except:
-                    raise EnvironmentError('Check .env file to see if you set <DATA_DIR> environment.')
-                data_dir = os.path.join(os.getcwd(), 'data_dir')
-            else:
-                raise ValueError(f'Unsupported value for the <API_SERVER_SCOPE> environment => {application_scope}')
-        except:
-            raise EnvironmentError('Check .env file to see if you set <API_SERVER_SCOPE> environment.')
+    #         if application_scope == 'DEVELOPMENT':
+    #             try:
+    #                 PORT = int(os.getenv('PORT'))
+    #                 if not PORT:
+    #                     raise EnvironmentError('<PORT> environment needs to be set.')
+    #             except:
+    #                 raise EnvironmentError('Check .env file to see if you set <DATA_DIR> environment.')
+    #             data_dir = os.path.join(os.getcwd(), 'data_dir')
+    #         else:
+    #             raise ValueError(f'Unsupported value for the <API_SERVER_SCOPE> environment => {application_scope}')
+    #     except:
+    #         raise EnvironmentError('Check .env file to see if you set <API_SERVER_SCOPE> environment.')
     
     os.environ['DATA_DIR'] = data_dir
 
     print('\n')
     print('~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~')
     print('~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~')
-    print('Application is up and running successfully.')
-    print('~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~')
+    print('...::: Trying to run the application :::...')
     print('~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~\n')
-    print(f'Application Scope => {application_scope}')
-    print(f'Running on Port => {PORT}')
+    print(f'Scope => {application_scope}')
+    print(f'Port => {PORT}')
     
     if application_scope == 'DEPLOYMENT':
         print('Volume Link =>')
@@ -78,4 +91,4 @@ if __name__ == "__main__":
     print('\n~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~')
     print('~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~\n')
 
-    uvicorn.run("fastapi_boilerplate.create_app:app", host='0.0.0.0', port=PORT, reload=True)
+    uvicorn.run("{{ cookiecutter.project_slug }}.create_app:app", host='0.0.0.0', port=PORT, reload=True)

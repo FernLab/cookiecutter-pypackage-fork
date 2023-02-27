f0e01d05405a3e8809d709e2105e7f874c7789fd
arashmad <arashmad@gfz-potsdam.de>
2023-02-27 15:10:56 +0100

    modify warning happening in make docs commands

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/env.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/env.py
@@ -19,11 +19,14 @@ if 'service_namespace' in os.environ:  # pragma: no cover
             Path(DATA_DIR).mkdir(parents=True, exist_ok=True)
 
 else:
-    RESULTS_DIR = os.environ['DATA_DIR']
-    if 'API_SERVER_SCOPE' in os.environ:
-        if os.environ['API_SERVER_SCOPE'] == 'DEVELOPMENT':
-            if not os.path.isdir(RESULTS_DIR):
-                try:
-                    os.mkdir(RESULTS_DIR)
-                except Exception as e:
-                    raise NotADirectoryError(f'Failed to create result directory => {e}')
+    try:
+        RESULTS_DIR = os.environ['DATA_DIR']
+        if 'API_SERVER_SCOPE' in os.environ:
+            if os.environ['API_SERVER_SCOPE'] == 'DEVELOPMENT':
+                if not os.path.isdir(RESULTS_DIR):
+                    try:
+                        os.mkdir(RESULTS_DIR)
+                    except Exception as e:
+                        raise NotADirectoryError(f'Failed to create result directory => {e}')
+    except Exception as e:
+        print(f'Warning => Not defined <DATA_DIR> environment. => {e}')

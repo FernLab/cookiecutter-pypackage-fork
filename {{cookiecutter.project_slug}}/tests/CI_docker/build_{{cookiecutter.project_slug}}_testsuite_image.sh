5bc58fd54b5864bdb80d87e0ef1550821b9047dd
Daniela Rabe <dara@gfz-potsdam.de>
2024-10-17 14:01:52 +0200

    fix comment line

--- {{cookiecutter.project_slug}}/tests/CI_docker/build_{{cookiecutter.project_slug}}_testsuite_image.sh
+++ {{cookiecutter.project_slug}}/tests/CI_docker/build_{{cookiecutter.project_slug}}_testsuite_image.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # SPDX-License-Identifier: {{ cookiecutter.open_source_license }}
 # FileType: SCRIPT
 # FileCopyrightText: {% now 'utc', '%Y' %}, {{ cookiecutter.full_name }} at GFZ Potsdam
